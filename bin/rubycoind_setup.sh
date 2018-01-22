73f03df9406b8dfbd3696814cab777f12ed68aae
Durendal <durendal@durendals-domain.com>
2018-01-22 17:31:04 -0600

    Added nodes to rubycoin.conf

--- bin/rubycoind_setup.sh
+++ bin/rubycoind_setup.sh
@@ -14,18 +14,33 @@ if [ -e "/rubycoin/rubycoin.conf" ]; then
 fi
 
 if [ -z ${ENABLE_WALLET:+x} ]; then
-    echo "disablewallet=1" >> "/rubycoin/rubycoin.conf"
+    echo "disablewallet=0" >> "/rubycoin/rubycoin.conf"
 fi
 
 if [ ! -z ${MAX_CONNECTIONS:+x} ]; then
     echo "maxconnections=${MAX_CONNECTIONS}" >> "/rubycoin/rubycoin.conf"
 fi
 
-if [ ! -z ${RPC_SERVER:+x} ]; then
-    RPC_USER=${RPC_USER:-rubycoinrpc}
-    RPC_PASSWORD=${RPC_PASSWORD:-$(dd if=/dev/urandom bs=20 count=1 2>/dev/null | base64)}
+#if [ ! -z ${RPC_SERVER:+x} ]; then
+RPC_USER=${RPC_USER:-rubycoinrpc}
+RPC_PASSWORD=${RPC_PASSWORD:-$(dd if=/dev/urandom bs=20 count=1 2>/dev/null | base64)}
 
-    echo "server=1" >> "/rubycoin/rubycoin.conf"
-    echo "rpcuser=${RPC_USER}" >> "/rubycoin/rubycoin.conf"
-    echo "rpcpassword=${RPC_PASSWORD}" >> "/rubycoin/rubycoin.conf"
-fi;
+echo "server=1" >> "/rubycoin/rubycoin.conf"
+echo "rpcuser=${RPC_USER}" >> "/rubycoin/rubycoin.conf"
+echo "rpcpassword=${RPC_PASSWORD}" >> "/rubycoin/rubycoin.conf"
+echo "addnode=104.156.233.202:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=45.32.239.220:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=146.185.166.144:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=104.238.136.238:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=76.74.178.137:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=69.121.160.102:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=46.4.119.238:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=109.169.71.17:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=78.129.241.145:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=45.76.198.141:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=138.197.135.225:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=73.120.64.249:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=14.202.55.175:5937" >> "/rubycoin/rubycoin.conf"
+echo "addnode=1.34.180.245:5937" >> "/rubycoin/rubycoin.conf"
+
+#fi;
